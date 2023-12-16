2dbe7e717e2e43d366da1d87f4d921135eab73a8
AlexDanilovich <alexeidanilovich031193@gmail.com>
2023-12-16 15:40:19 +0100

    feat: added background script, fixed content script

--- public/content.js
+++ public/content.js
@@ -1,14 +1,8 @@
-setTimeout(() => {
-  const manager = window.__MOBX_STORE_MANAGER__;
+const manager = window.__MOBX_STORE_MANAGER__;
 
-  if (typeof manager === 'undefined') {
-    return;
-  }
-
-  // console.log('manager:', manager);
-
+if (manager) {
   manager.__devOnChange = (detail) => {
     // console.log('- dispatchEvent DEV_ON_CHANGE -');
     document.dispatchEvent(new CustomEvent('DEV_ON_CHANGE', { detail }));
   };
-}, 1000);
+}
